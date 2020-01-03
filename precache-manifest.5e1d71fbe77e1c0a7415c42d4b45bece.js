fe9e4891f88b7623c1d19124d4a871548696266e
Colm McSharry <colmmcsharry@hotmail.com>
2020-01-03 00:16:47 -0800

    Updates

---
+++ precache-manifest.5e1d71fbe77e1c0a7415c42d4b45bece.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07960c6369b0511c0abfcbb05c7eb4c9",
+    "url": "/Testproj/index.html"
+  },
+  {
+    "revision": "c2923c2ebca754e75f25",
+    "url": "/Testproj/static/css/2.833dd627.chunk.css"
+  },
+  {
+    "revision": "bb66e2fdb846e4ae64c6",
+    "url": "/Testproj/static/css/main.660e4930.chunk.css"
+  },
+  {
+    "revision": "c2923c2ebca754e75f25",
+    "url": "/Testproj/static/js/2.923da71f.chunk.js"
+  },
+  {
+    "revision": "1977d94acb8517e64079a472548bc889",
+    "url": "/Testproj/static/js/2.923da71f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "bb66e2fdb846e4ae64c6",
+    "url": "/Testproj/static/js/main.bbcad7e1.chunk.js"
+  },
+  {
+    "revision": "a9028170c9f1f3b10a09",
+    "url": "/Testproj/static/js/runtime-main.f6b8adf4.js"
+  },
+  {
+    "revision": "5dcc161e93905bead8c2fdbb5ea9e200",
+    "url": "/Testproj/static/media/cabinforest.5dcc161e.jpg"
+  },
+  {
+    "revision": "d77ae6aad853648ae701b7a34082f645",
+    "url": "/Testproj/static/media/cabinlakeshopped2.d77ae6aa.jpg"
+  },
+  {
+    "revision": "113fd8cdac686269635460816e9c9c6f",
+    "url": "/Testproj/static/media/cabintriangle.113fd8cd.jpg"
+  },
+  {
+    "revision": "92293ff91a5ecf52705e716097cda334",
+    "url": "/Testproj/static/media/cabinwindow.92293ff9.jpg"
+  },
+  {
+    "revision": "61c4e0007254eb55e4691291881a4bfb",
+    "url": "/Testproj/static/media/clouds.61c4e000.png"
+  },
+  {
+    "revision": "9805ad57362aebd0c32867ac83f9232a",
+    "url": "/Testproj/static/media/screenshot2.9805ad57.png"
+  },
+  {
+    "revision": "dc5c90b34c1174784aa0ea5f3f0a2d7c",
+    "url": "/Testproj/static/media/yellowcabin.dc5c90b3.jpg"
+  }
+]);
