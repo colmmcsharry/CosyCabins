db91acfb2d2f9bdfcb5b752514a4b10d0271ad8f
Colm McSharry <colmmcsharry@hotmail.com>
2020-01-14 22:47:02 -0800

    header show on scroll

--- src/Navbar.js
+++ src/Navbar.js
@@ -6,9 +6,23 @@ import Smalldrop from "./Smalldrop";
 import NewdropSml from "./NewdropSml";
 
 export default class Navbar extends React.Component {
+
+
+
+
   render() {
+    var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("Mynavbar").style.top = "0";
+  } else {
+    document.getElementById("Mynavbar").style.top = "-150px";
+  }
+  prevScrollpos = currentScrollPos;
+}
     return (
-      <div className="Mynavbar">
+      <div id="Mynavbar">
         <div className="logocontainer">
           <Link to="/">
             <img src={logo} className="logo" alt="our logo"
